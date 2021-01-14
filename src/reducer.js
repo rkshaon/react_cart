58febac707b3635a3db1f06c19902e07ae7065c3
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-01-14 15:53:55 +0600

    decrease item

--- src/reducer.js
+++ src/reducer.js
@@ -17,6 +17,15 @@ const reducer = (state, action) => {
     });
     return {...state, cart: tempCart};
   }
+  if (action.type === 'DECREASE') {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return {...cartItem, amount: cartItem.amount - 1}
+      }
+      return cartItem;
+    });
+    return {...state, cart: tempCart};
+  }
   return state;
 }
 
